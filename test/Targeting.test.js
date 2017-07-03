2fcc51202b119a6682c322f8c59b6e2779d9b13b
Amit Tal <amit@talfamily.com>
2017-07-03 19:57:11 +0300

    test(Targeting): add targeting and matchers tests

---
+++ test/Targeting.test.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const { expect } = require('chai')
+const { Targeting, matchers } = require('../index')
+
+describe('Targeting',  () => {
+
+	describe('match', () => {
+
+		describe('one condition - without matcher', () => {
+			let targeting = new Targeting({ geo: 'US' })
+			it('visitor is targeted', () => {
+				expect(targeting.match({ geo: 'US' })).to.be.true
+			})
+		})
+
+		describe('one condition', () => {
+			let targeting = new Targeting({ geo: matchers.specific('US') })
+			let usVisitor = { geo: 'US' }
+			let ilVisitor = { geo: 'IL' }
+
+			it('visitor is targeted', () => {
+				expect(targeting.match(usVisitor)).to.be.true
+			})
+
+			it('visitor is not targeted', () => {
+				expect(targeting.match(ilVisitor)).to.be.false
+			})
+		})
+
+		describe('all conditions', () => {
+			let targeting = new Targeting({ geo: matchers.specific('US'), page: matchers.specific('buy') })
+			let usVisitor = { geo: 'US' , page: 'buy'}
+			let ilVisitor = { geo: 'IL' , page: 'buy'}
+
+			it('visitor is targeted', () => {
+				expect(targeting.match(usVisitor)).to.be.true
+			})
+
+			it('visitor is not targeted', () => {
+				expect(targeting.match(ilVisitor)).to.be.false
+			})
+		})
+	})
+})
