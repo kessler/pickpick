1014d9b8ee81832a70d2996e3651344cc1602506
kessler <yanivk@gmail.com>
2018-07-15 03:55:54 +0300

    remove all code and reference to matchers.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports.Experiment = require('./lib/Experiment')
 module.exports.Targeting = require('./lib/Targeting')
 module.exports.Variation = require('./lib/Variation')
-module.exports.matchers = require('./lib/matchers')
 module.exports.ExperimentContainer = require('./lib/ExperimentContainer')
